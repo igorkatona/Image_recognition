db854bac462592525e8728dbf9a7e11a7a6d6080
igorkatona <mustang559@gmail.com>
2022-03-26 11:23:31 +0400

    Added firt py code to project

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+# This our hello messege
+
+teamMembers= ['Igor', 'Amr Ibrahim', 'Rahul', 'Al Shayma']
+
+for values in teamMembers:
+    print( "Hello " + values)
+
+print("Our team name is Raashi")
