64164747356aff791614977835c1ec59c84b02cd
igorkatona <102136248+igorkatona@users.noreply.github.com>
2022-04-07 16:33:40 +0400

    Delete helloworld.py

--- helloworld.py
+++
@@ -1,8 +0,0 @@
-# This our hello messege
-
-teamMembers= ['Igor', 'Amr Ibrahim', 'Rahul', 'Al Shayma']
-
-for values in teamMembers:
-    print( "Hello " + values)
-
-print("Our team name is Raashi")
